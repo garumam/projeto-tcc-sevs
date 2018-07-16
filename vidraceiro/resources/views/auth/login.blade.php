db97ab728761814a5136af5349d7ab6992f276fb
Lucas <freirejandre@gmail.com>
2018-07-16 20:42:10 -0300

    auth

--- vidraceiro/resources/views/auth/login.blade.php
+++ vidraceiro/resources/views/auth/login.blade.php
@@ -1,71 +1,54 @@
-@extends('layouts.app')
+@extends('layouts.layoutauth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+    <section id="login">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <form id="formulario" class="card-shadow-2dp" role="form" method="POST"
+                          action="{{ route('login') }}" aria-label="{{ __('Login') }}">
                         @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="text-center mb-4">
+                            <img class="" src="{{ asset('img/bootstrap-solid.svg') }}" alt="" width="72" height="72">
+                            <!-- <h1 class="title">Vidraceiro</h1> -->
+                        </div>
+                        <h1 class="title">{{ __('Login') }}</h1>
+                        <div class="form-label-group">
+                            <input type="email" id="email"
+                                   class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
+                                   value="{{ old('email') }}" placeholder="Email address" required autofocus>
+                            <label for="email">{{ __('E-Mail Address') }}</label>
+                            @if($errors->has('email'))
+                                <span class="badge badge-danger mt-3">{{ $errors->first('email') }}</span>
+                            @endif
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="form-label-group">
+                            <input type="password" id="password"
+                                   class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}"
+                                   name="password" placeholder="Password" required>
+                            <label for="password">{{ __('Password') }}</label>
+                            @if ($errors->has('password'))
+                                <span class="badge badge-danger mt-3">{{ $errors->first('password') }}</span>
+                            @endif
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
+                        <div class="checkbox mb-3">
+                            <label>
+                                <input type="checkbox" value="remember" name="remember"
+                                       id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                {{ __('Remember Me') }}
+                            </label>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
+                        <button class="btn btn-lg btn-primary btn-block btn-custom"
+                                type="submit">{{ __('Login') }}</button>
+                        <!-- <p class="mt-5 mb-3 text-muted text-center">&copy; 2018</p> -->
                     </form>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+        <a class="btn btn-link text-muted text text-center w-100" href="{{ route('password.request') }}">
+            {{ __('Forgot Your Password?') }}
+        </a>
+    </section>
 @endsection
---
+++ vidraceiro/resources/views/layouts/layoutauth.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+{{--<script src="{{ asset('js/app.js') }}" defer></script>--}}
+
+<!-- Fonts -->
+    {{--<link rel="dns-prefetch" href="https://fonts.gstatic.com">--}}
+    {{--<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">--}}
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('css/reset.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+
+    {{--<link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
+</head>
+<body>
+<div id="app">
+    <main class="py-4">
+        @yield('content')
+    </main>
+</div>
+
+<script src="{{ asset('js/jquery.min.js') }}" defer></script>
+<script src="{{ asset('js/popper.js') }}" defer></script>
+<script src="{{ asset('js/bootstrap.min.js') }}" defer></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="{{ asset('js/ie10-viewport-bug-workaround.js') }}" defer></script>
+</body>
+</html>
