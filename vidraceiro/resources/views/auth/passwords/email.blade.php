6f27cd2cf43369906c797c6c3e9940cb9c780487
Lucas <freirejandre@gmail.com>
2018-07-16 22:46:29 -0300

    auth reset password

--- vidraceiro/resources/views/auth/passwords/email.blade.php
+++ vidraceiro/resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,30 @@
-@extends('layouts.app')
+@extends('layouts.layoutauth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}" aria-label="{{ __('Reset Password') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+    <form id="formulario" class="card-shadow-2dp" role="form" method="POST" action="{{ route('password.email') }}"
+          aria-label="{{ __('Reset Password') }}">
+        @csrf
+        <div class="text-center mb-4">
+            <img class="" src="{{ asset('img/bootstrap-solid.svg') }}" alt="" width="72" height="72">
+            <!-- <h1 class="title">Vidraceiro</h1> -->
         </div>
-    </div>
-</div>
+        <h1 class="title">{{ __('Reset Password') }}</h1>
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
+        <div class="form-label-group">
+            <input type="email" id="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                   name="email" value="{{ old('email') }}" placeholder="Email address" required autofocus>
+            <label for="email">{{ __('E-Mail Address') }}</label>
+            @if ($errors->has('email'))
+                <span class="badge badge-danger mt-3">{{ $errors->first('email') }}</span>
+            @endif
+        </div>
+        <button class="btn btn-lg btn-primary btn-block btn-custom"
+                type="submit"> {{ __('Send Password Reset Link') }}</button>
+        <!-- <p class="mt-5 mb-3 text-muted text-center">&copy; 2018</p> -->
+    </form>
 @endsection
