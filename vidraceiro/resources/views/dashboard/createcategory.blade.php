87e32e2a8f91eb0d05785821f92d13453eb3d4f5
Lucas <freirejandre@gmail.com>
2018-07-22 20:51:37 -0300

    criando views

---
+++ vidraceiro/resources/views/dashboard/createcategory.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+@section('content')
+
+    <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="card-material custom-card">
+
+            <div class="topo">
+                <h4 class="titulo">Adicionar Usuarios </h4>
+            </div>
+
+            <form class="formulario" method="POST" role="form">
+                <div class="form-row">
+
+                    <div class="form-group col-md-4">
+                        <label for="nome">Nome</label>
+                        <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome"> </input>
+                    </div>
+
+                    <div class="form-group col-md-4">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" name="password"
+                               placeholder="Password">
+                    </div>
+
+                    <div class="form-group col-md-4">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" name="password"
+                               placeholder="Password">
+                    </div>
+
+                    <div class="form-group col-md-4">
+                        <label for="nome">Nome</label>
+                        <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome"> </input>
+                    </div>
+
+
+                </div>
+                <button class="btn btn-lg btn-primary btn-block btn-custom w-3277" type="submit">Enviar</button>
+
+            </form>
+        </div>
+    </div>
+@endsection
