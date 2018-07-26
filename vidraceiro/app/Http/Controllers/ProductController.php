97bd82d553149cb306143dc65db52c0807843c13
Lucas <freirejandre@gmail.com>
2018-07-26 02:04:55 -0300

    pegando nomes das imagens

--- vidraceiro/app/Http/Controllers/ProductController.php
+++ vidraceiro/app/Http/Controllers/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -17,18 +19,55 @@ class ProductController extends Controller
     }
 
 
-    public function create(){
+    public function create()
+    {
+//        $nomes = [];
+//        $novonome = [];
+//        $files = File::files(public_path().'/img/boxdiversos/');
+//        foreach($files as $path)
+//        {
+//            $nomes[] = pathinfo($path);
+//        }
+//        for ($i = 0; $i < count($files); $i++){
+//            $novonome[$i] = $nomes[$i]['filename'];
+//        }
+        $boxdiversos = $this->retornaNomes('/img/boxdiversos/');
+        $boxpadrao = $this->retornaNomes('/img/boxpadrao/');
+        $ferragem1000 = $this->retornaNomes('/img/ferragem1000/');
+        $ferragem3000 = $this->retornaNomes('/img/ferragem3000/');
 
-        return view('dashboard.create.product')->with('title', 'Criar Produto');
+
+//        var_dump($boxdiversos,$boxpadrao,$ferragem1000,$ferragem3000);
+        return view('dashboard.create.product', compact('boxdiversos'))->with('title', 'Criar Produto');
 
     }
-    public function read(){
+
+    public function retornaNomes($folder)
+    {
+        $nomes = [];
+        $novonome = [];
+        $files = File::files(public_path() . $folder);
+        foreach ($files as $path) {
+            $nomes[] = pathinfo($path);
+        }
+        for ($i = 0; $i < count($files); $i++) {
+            $novonome[$i] = $nomes[$i]['filename'];
+        }
+        return $novonome;
+    }
+
+    public function read()
+    {
 
     }
-    public function update(){
+
+    public function update()
+    {
 
     }
-    public function delete(){
+
+    public function delete()
+    {
 
     }
 }
--- vidraceiro/resources/views/dashboard/create/product.blade.php
+++ vidraceiro/resources/views/dashboard/create/product.blade.php
@@ -7,27 +7,34 @@
             <!-- Inicio tab de Produto-->
             <nav>
                 <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                    <a class="nav-item nav-link active" id="nav-adicionar-produto-tab" data-toggle="tab" href="#nav-adicionar-produto" role="tab" aria-controls="nav-adicionar-produto" aria-selected="true">Adicionar Produto</a>
-                    <a class="nav-item nav-link" id="nav-adicionar-material-tab" data-toggle="tab" href="#nav-adicionar-material" role="tab" aria-controls="nav-adicionar-material" aria-selected="false">Adicionar Material</a>
+                    <a class="nav-item nav-link active" id="nav-adicionar-produto-tab" data-toggle="tab"
+                       href="#nav-adicionar-produto" role="tab" aria-controls="nav-adicionar-produto"
+                       aria-selected="true">Adicionar Produto</a>
+                    <a class="nav-item nav-link" id="nav-adicionar-material-tab" data-toggle="tab"
+                       href="#nav-adicionar-material" role="tab" aria-controls="nav-adicionar-material"
+                       aria-selected="false">Adicionar Material</a>
                 </div>
             </nav>
             <!-- Fim tab de Produto-->
 
             <!--Inicio Conteudo de cada tab -->
             <div class="tab-content" id="nav-tabContent">
-                <div class="tab-pane fade show active" id="nav-adicionar-produto" role="tabpanel" aria-labelledby="nav-adicionar-produto-tab">
+                <div class="tab-pane fade show active" id="nav-adicionar-produto" role="tabpanel"
+                     aria-labelledby="nav-adicionar-produto-tab">
 
                     <form class="formulario" method="POST" role="form">
                         <div class="form-row">
 
                             <div class="form-group col-md-4">
                                 <label>Nome</label>
-                                <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome" required>
+                                <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome"
+                                       required>
                             </div>
 
                             <div class="form-group col-md-4">
                                 <label>Descrição</label>
-                                <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Descrição" required>
+                                <input type="text" class="form-control" id="descricao" name="descricao"
+                                       placeholder="Descrição" required>
                             </div>
 
                             <div class="form-group col-md-4">
@@ -42,34 +49,43 @@
 
                             <div class="form-group col-md-4">
 
-                                <img id="image-selecionar2" src="{{ asset('img/semimagem.png') }}" class="img-fluid" alt="Responsive image">
-                                <a href="#" class="btn btn-lg btn-primary btn-custom w-3277" data-toggle="modal" data-target="#imagensModal">Buscar</a>
-                                <label for="url-image2"></label><input type="text" id="url-image2" name="url-image2" style="display: none;">
+                                <img id="image-selecionar2" src="{{ asset('img/semimagem.png') }}" class="img-fluid"
+                                     alt="Responsive image">
+                                <a href="#" class="btn btn-lg btn-primary btn-custom w-3277" data-toggle="modal"
+                                   data-target="#imagensModal">Buscar</a>
+                                <label for="url-image2"></label><input type="text" id="url-image2" name="url-image2"
+                                                                       style="display: none;">
                             </div>
 
                             <div class="form-group col-md-4">
                                 <label>Imagem</label>
 
                                 <div id="dropmenu-imagem" class="dropdown">
-                                    <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">
-                                        <img id="image-selecionar" src="{{ asset('img/bootstrap-solid.svg')}}" class="img-fluid" alt="Responsive image">
+                                    <button class="btn btn-default dropdown-toggle" type="button" id="menu1"
+                                            data-toggle="dropdown">
+                                        <img id="image-selecionar" src="{{ asset('img/bootstrap-solid.svg')}}"
+                                             class="img-fluid" alt="Responsive image">
                                     </button>
-                                    <label for="url-image"></label><input type="text" id="url-image" name="url-image" style="display: none;">
+                                    <label for="url-image"></label><input type="text" id="url-image" name="url-image"
+                                                                          style="display: none;">
                                     <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
 
                                         <li role="presentation">
                                             {{--<a role="menuitem" tabindex="-1" href="#">--}}
-                                                <img src="{{ asset('img/boxdiversos/bxa1.png')}}" class="img-fluid" alt="Responsive image">
+                                            <img src="{{ asset('img/boxdiversos/bxa1.png')}}" class="img-fluid"
+                                                 alt="Responsive image">
                                             {{--</a>--}}
                                         </li>
                                         <li role="presentation">
                                             {{--<a role="menuitem" tabindex="-1" href="#">--}}
-                                                <img src="{{ asset('img/boxdiversos/bxa2.png')}}" class="img-fluid" alt="Responsive image">
+                                            <img src="{{ asset('img/boxdiversos/bxa2.png')}}" class="img-fluid"
+                                                 alt="Responsive image">
                                             {{--</a>--}}
                                         </li>
                                         <li role="presentation">
                                             {{--<a role="menuitem" tabindex="-1" href="#">--}}
-                                                <img src="{{ asset('img/boxdiversos/bxc1.png')}}" class="img-fluid" alt="Responsive image">
+                                            <img src="{{ asset('img/boxdiversos/bxc1.png')}}" class="img-fluid"
+                                                 alt="Responsive image">
                                             {{--</a>--}}
                                         </li>
                                     </ul>
@@ -84,14 +100,16 @@
                     </form>
 
                 </div>
-                <div class="tab-pane fade" id="nav-adicionar-material" role="tabpanel" aria-labelledby="nav-adicionar-material-tab">
+                <div class="tab-pane fade" id="nav-adicionar-material" role="tabpanel"
+                     aria-labelledby="nav-adicionar-material-tab">
 
                     <form class="formulario" method="POST" role="form">
                         <div class="form-row">
 
                             <div class="form-group col-md-4">
                                 <label for="nome">Nome</label>
-                                <input type="text" class="form-control" id="nome_material" name="nome_material" placeholder="Nome">
+                                <input type="text" class="form-control" id="nome_material" name="nome_material"
+                                       placeholder="Nome">
                             </div>
 
                             <div class="form-group col-md-4">
@@ -108,7 +126,6 @@
                 </div>
             </div>
             <!--Inicio Conteudo de cada tab -->
-
 
 
         </div>
@@ -130,27 +147,24 @@
                 <div class="modal-body">
 
                     <div class="container-fluid">
-                        <div id="gridImagens" class="row justify-content-center align-items-center">
+                        <div id="gridImagens" class="row">
+                            @for($i = 0; $i < count($boxdiversos); $i++ )
+                                <div class="col-">
+                                    <img src="{{ asset('img/boxdiversos/'.$boxdiversos[$i].'.png')}}" class="img-fluid img-thumbnail"
+                                         alt="Responsive image">
+                                </div>
+                            @endfor
 
-                            <div class="col-auto">
-
-                                <img src="{{ asset('img/boxdiversos/bxa1.png')}}" class="img-fluid img-thumbnail" alt="Responsive image">
-
-                            </div>
-
-
-                            <div class="col-auto">
-
-                                <img src="{{ asset('img/boxdiversos/bxa2.png')}}" class="img-fluid img-thumbnail" alt="Responsive image">
-
-                            </div>
+                            {{--<div class="col-">--}}
+                                {{--<img src="{{ asset('img/boxdiversos/bxa2.png')}}" class="img-fluid img-thumbnail"--}}
+                                     {{--alt="Responsive image">--}}
+                            {{--</div>--}}
 
 
-                            <div class="col-auto">
-
-                                <img src="{{ asset('img/boxdiversos/bxc1.png')}}" class="img-fluid img-thumbnail" alt="Responsive image">
-
-                            </div>
+                            {{--<div class="col-">--}}
+                                {{--<img src="{{ asset('img/boxdiversos/bxc1.png')}}" class="img-fluid img-thumbnail"--}}
+                                     {{--alt="Responsive image">--}}
+                            {{--</div>--}}
 
 
                         </div>
