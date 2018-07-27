05a3a205bb4ac0df4ca628d450658dd3fc6b3d20
Lucas <freirejandre@gmail.com>
2018-07-26 23:10:33 -0300

    categorias

--- vidraceiro/public/js/dashboard.js
+++ vidraceiro/public/js/dashboard.js
@@ -45,6 +45,7 @@ $(document).ready(function () {
         boxpadrao.css("display", "none");
         ferragem1000.css("display", "none");
         ferragem3000.css("display", "none");
+        selecionecategoria.css("display", "none");
 
         switch (valueSelected) {
             case "boxdiversos":
