16ba2f5bf6d57ad00031066d9ce632fd62b772d5
Lucas <freirejandre@gmail.com>
2018-07-31 19:21:43 -0300

    prefixo nas rotas

--- vidraceiro/routes/web.php
+++ vidraceiro/routes/web.php
@@ -18,100 +18,107 @@ Route::get('/', function () {
 });
 
 Route::get('/home', 'DashboardController@index')->name('home');
-Route::get('/list','DashboardController@list')->name('list');
-
+Route::get('/list', 'DashboardController@list')->name('list');
 
 
 //rotas do usuario
-Route::get('/users','UserController@index')->name('users.index');
-Route::get('/users/create','UserController@create')->name('users.create');
-Route::post('/users','UserController@store')->name('users.store');
-Route::get('/users/{user}','UserController@show')->name('users.show');
-Route::get('/users/{id}/edit','UserController@edit')->name('users.edit');
-Route::patch('/users/{id}','UserController@update')->name('users.update');
-Route::delete('/users/{id}','UserController@destroy')->name('users.destroy');
+Route::prefix('users')->group(function () {
+    Route::get('/', 'UserController@index')->name('users.index');
+    Route::get('/create', 'UserController@create')->name('users.create');
+    Route::post('/', 'UserController@store')->name('users.store');
+    Route::get('/{user}', 'UserController@show')->name('users.show');
+    Route::get('/{id}/edit', 'UserController@edit')->name('users.edit');
+    Route::patch('/{id}', 'UserController@update')->name('users.update');
+    Route::delete('/{id}', 'UserController@destroy')->name('users.destroy');
+});
 
 //rotas de produto
-Route::get('/products','ProductController@index')->name('products.index');
-Route::get('/products/create','ProductController@create')->name('products.create');
-Route::post('/products','ProductController@store')->name('products.store');
-Route::get('/products/{user}','ProductController@show')->name('products.show');
-Route::get('/products/{id}/edit','ProductController@edit')->name('products.edit');
-Route::patch('/products/{id}','ProductController@update')->name('products.update');
-Route::delete('/products/{id}','ProductController@destroy')->name('products.destroy');
-
+Route::prefix('products')->group(function () {
+    Route::get('/', 'ProductController@index')->name('products.index');
+    Route::get('/create', 'ProductController@create')->name('products.create');
+    Route::post('/', 'ProductController@store')->name('products.store');
+    Route::get('/{user}', 'ProductController@show')->name('products.show');
+    Route::get('/{id}/edit', 'ProductController@edit')->name('products.edit');
+    Route::patch('/{id}', 'ProductController@update')->name('products.update');
+    Route::delete('/{id}', 'ProductController@destroy')->name('products.destroy');
+});
 
 //rotas de categoria
-
-Route::get('/categories','CategoryController@index')->name('categories.index');
-Route::get('/categories/create','CategoryController@create')->name('categories.create');
-Route::post('/categories','CategoryController@store')->name('categories.store');
-Route::get('/categories/{user}','CategoryController@show')->name('categories.show');
-Route::get('/categories/{id}/edit','CategoryController@edit')->name('categories.edit');
-Route::patch('/categories/{id}','CategoryController@update')->name('categories.update');
-Route::delete('/categories/{id}','CategoryController@destroy')->name('categories.destroy');
-
+Route::prefix('categories')->group(function () {
+    Route::get('/', 'CategoryController@index')->name('categories.index');
+    Route::get('/create', 'CategoryController@create')->name('categories.create');
+    Route::post('/', 'CategoryController@store')->name('categories.store');
+    Route::get('/{user}', 'CategoryController@show')->name('categories.show');
+    Route::get('/{id}/edit', 'CategoryController@edit')->name('categories.edit');
+    Route::patch('/{id}', 'CategoryController@update')->name('categories.update');
+    Route::delete('/{id}', 'CategoryController@destroy')->name('categories.destroy');
+});
 
 //rotas dos materiais
-Route::get('/materials','MaterialController@index')->name('materials.index');
-Route::get('/materials/create','MaterialController@create')->name('materials.create');
-Route::post('/materials','MaterialController@store')->name('materials.store');
-Route::get('/materials/{user}','MaterialController@show')->name('materials.show');
-Route::get('/materials/{id}/edit','MaterialController@edit')->name('materials.edit');
-Route::patch('/materials/{id}','MaterialController@update')->name('materials.update');
-Route::delete('/materials/{id}','MaterialController@destroy')->name('materials.destroy');
-
-
-
+Route::prefix('materials')->group(function () {
+    Route::get('/', 'MaterialController@index')->name('materials.index');
+    Route::get('/create', 'MaterialController@create')->name('materials.create');
+    Route::post('', 'MaterialController@store')->name('materials.store');
+    Route::get('/{user}', 'MaterialController@show')->name('materials.show');
+    Route::get('/{id}/edit', 'MaterialController@edit')->name('materials.edit');
+    Route::patch('/{id}', 'MaterialController@update')->name('materials.update');
+    Route::delete('/{id}', 'MaterialController@destroy')->name('materials.destroy');
+});
 
 //rotas dos orçamentos
-Route::get('/budgets','BudgetController@index')->name('budgets.index');
-Route::get('/budgets/create','BudgetController@create')->name('budgets.create');
-Route::post('/budgets','BudgetController@store')->name('budgets.store');
-Route::get('/budgets/{user}','BudgetController@show')->name('budgets.show');
-Route::get('/budgets/{id}/edit','BudgetController@edit')->name('budgets.edit');
-Route::patch('/budgets/{id}','BudgetController@update')->name('budgets.update');
-Route::delete('/budgets/{id}','BudgetController@destroy')->name('budgets.destroy');
-
+Route::prefix('budgets')->group(function () {
+    Route::get('/', 'BudgetController@index')->name('budgets.index');
+    Route::get('/create', 'BudgetController@create')->name('budgets.create');
+    Route::post('/', 'BudgetController@store')->name('budgets.store');
+    Route::get('/{user}', 'BudgetController@show')->name('budgets.show');
+    Route::get('/{id}/edit', 'BudgetController@edit')->name('budgets.edit');
+    Route::patch('/{id}', 'BudgetController@update')->name('budgets.update');
+    Route::delete('/{id}', 'BudgetController@destroy')->name('budgets.destroy');
+});
 
 //rotas das ordens de serviço
-Route::get('/orders','OrderController@index')->name('orders.index');
-Route::get('/orders/create','OrderController@create')->name('orders.create');
-Route::post('/orders','OrderController@store')->name('orders.store');
-Route::get('/orders/{user}','OrderController@show')->name('orders.show');
-Route::get('/orders/{id}/edit','OrderController@edit')->name('orders.edit');
-Route::patch('/orders/{id}','OrderController@update')->name('orders.update');
-Route::delete('/orders/{id}','OrderController@destroy')->name('orders.destroy');
+Route::prefix('orders')->group(function () {
+    Route::get('/', 'OrderController@index')->name('orders.index');
+    Route::get('/create', 'OrderController@create')->name('orders.create');
+    Route::post('/', 'OrderController@store')->name('orders.store');
+    Route::get('/{user}', 'OrderController@show')->name('orders.show');
+    Route::get('/{id}/edit', 'OrderController@edit')->name('orders.edit');
+    Route::patch('/{id}', 'OrderController@update')->name('orders.update');
+    Route::delete('/{id}', 'OrderController@destroy')->name('orders.destroy');
+});
 
 //rotas das fornecedores
-Route::get('/providers','ProviderController@index')->name('providers.index');
-Route::get('/providers/create','ProviderController@create')->name('providers.create');
-Route::post('/providers','ProviderController@store')->name('providers.store');
-Route::get('/providers/{user}','ProviderController@show')->name('providers.show');
-Route::get('/providers/{id}/edit','ProviderController@edit')->name('providers.edit');
-Route::patch('/providers/{id}','ProviderController@update')->name('providers.update');
-Route::delete('/providers/{id}','ProviderController@destroy')->name('providers.destroy');
-
+Route::prefix('providers')->group(function () {
+    Route::get('/', 'ProviderController@index')->name('providers.index');
+    Route::get('/create', 'ProviderController@create')->name('providers.create');
+    Route::post('/', 'ProviderController@store')->name('providers.store');
+    Route::get('/{user}', 'ProviderController@show')->name('providers.show');
+    Route::get('/{id}/edit', 'ProviderController@edit')->name('providers.edit');
+    Route::patch('/{id}', 'ProviderController@update')->name('providers.update');
+    Route::delete('/{id}', 'ProviderController@destroy')->name('providers.destroy');
+});
 
 //rotas da empresa
-Route::get('/companies','CompanyController@index')->name('companies.index');
-Route::get('/companies/create','CompanyController@create')->name('companies.create');
-Route::post('/companies','CompanyController@store')->name('companies.store');
-Route::get('/companies/{user}','CompanyController@show')->name('companies.show');
-Route::get('/companies/{id}/edit','CompanyController@edit')->name('companies.edit');
-Route::patch('/companies/{id}','CompanyController@update')->name('companies.update');
-Route::delete('/companies/{id}','CompanyController@destroy')->name('companies.destroy');
-
+Route::prefix('companies')->group(function () {
+    Route::get('/', 'CompanyController@index')->name('companies.index');
+    Route::get('/create', 'CompanyController@create')->name('companies.create');
+    Route::post('/', 'CompanyController@store')->name('companies.store');
+    Route::get('/{user}', 'CompanyController@show')->name('companies.show');
+    Route::get('/{id}/edit', 'CompanyController@edit')->name('companies.edit');
+    Route::patch('/{id}', 'CompanyController@update')->name('companies.update');
+    Route::delete('/{id}', 'CompanyController@destroy')->name('companies.destroy');
+});
 
 //rotas da gerar pdf
-Route::get('/pdf','PdfController@index')->name('pdf.index');
-Route::get('/pdf/create','PdfController@create')->name('pdf.create');
-Route::post('/pdf','PdfController@store')->name('pdf.store');
-Route::get('/pdf/{user}','PdfController@show')->name('pdf.show');
-Route::get('/pdf/{id}/edit','PdfController@edit')->name('pdf.edit');
-Route::patch('/pdf/{id}','PdfController@update')->name('pdf.update');
-Route::delete('/pdf/{id}','PdfController@destroy')->name('pdf.destroy');
-
+Route::prefix('pdf')->group(function () {
+    Route::get('/', 'PdfController@index')->name('pdf.index');
+    Route::get('/create', 'PdfController@create')->name('pdf.create');
+    Route::post('/', 'PdfController@store')->name('pdf.store');
+    Route::get('/{user}', 'PdfController@show')->name('pdf.show');
+    Route::get('/{id}/edit', 'PdfController@edit')->name('pdf.edit');
+    Route::patch('/{id}', 'PdfController@update')->name('pdf.update');
+    Route::delete('/{id}', 'PdfController@destroy')->name('pdf.destroy');
+});
 
 // Authentication Routes...
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
