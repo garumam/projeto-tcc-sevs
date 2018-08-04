e47db6e5e1199b6ffb6a14aed191ef22ae5dfcfd
CASA <felipe.epr.faculdade@gmail.com>
2018-08-04 00:38:10 -0300

    metodo entre vidro e categoria

--- vidraceiro/app/Category.php
+++ vidraceiro/app/Category.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Glass;
 class Category extends Model
 {
-    //
+    public function glasses(){
+        return $this->hasMany(
+            Glass::class,
+            'categoria_vidro_id'
+        );
+    }
 }
--- vidraceiro/app/Glass.php
+++ vidraceiro/app/Glass.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Product;
+use App\Category;
 class Glass extends Model
 {
     public function products(){
@@ -12,6 +13,13 @@ class Glass extends Model
             'product_glass',
             'vidro_id',
             'produto_id'
+        );
+    }
+
+    public function category(){
+        return $this->belongsTo(
+            Category::class,
+            'categoria_vidro_id'
         );
     }
 }
