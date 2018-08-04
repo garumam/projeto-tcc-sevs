3dc883b0252f9bb9640827ec5908187c19c18fab
CASA <felipe.epr.faculdade@gmail.com>
2018-08-04 00:02:31 -0300

    metodo de produto e vidro

--- vidraceiro/app/Glass.php
+++ vidraceiro/app/Glass.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Product;
 class Glass extends Model
 {
-    //
+    public function products(){
+        return $this->belongsToMany(
+            Product::class,
+            'product_glass',
+            'vidro_id',
+            'produto_id'
+        );
+    }
 }
--- vidraceiro/app/Product.php
+++ vidraceiro/app/Product.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Glass;
 
 class Product extends Model
 {
-    //
+    public function glasses(){
+        return $this->belongsToMany(
+            Glass::class,
+            'product_glass',
+            'produto_id',
+            'vidro_id'
+        );
+    }
 }
