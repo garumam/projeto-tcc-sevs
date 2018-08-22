15b54ab0ede0224a6e9571e9239ba77057556e5a
Lucas <freirejandre@gmail.com>
2018-08-22 01:01:51 -0300

    pdf

--- vidraceiro/resources/views/dashboard/pdf/budget.blade.php
+++ vidraceiro/resources/views/dashboard/pdf/budget.blade.php
@@ -7,7 +7,7 @@
     <!--Custon CSS (está em /public/assets/site/css/certificate.css)-->
     {{--<link rel="stylesheet" href="{{ url('assets/site/css/certificate.css') }}">--}}
     <style>
-        p,h3 {
+        p, h3 {
             font-weight: 700;
             font-family: 'Raleway', sans-serif;
         }
@@ -16,10 +16,42 @@
             border-bottom: 2px solid #e5e5e5;
             height: 2px;
         }
+
         h3 {
             background-color: #DFEBFF;
             padding: .4rem;
         }
+
+        .flex {
+            width: 100%;
+        }
+
+        .image-produto {
+            position: relative;
+            display: block;
+            margin-top: 20px;
+            width: 15%;
+            height: 120px;
+        }
+
+        .tabela-produto {
+            margin-top: 13px;
+            width: 80%;
+            height: 120px;
+            float: right;
+            border: 1px solid #1b1e21;
+            font-family: 'Raleway', sans-serif;
+            font-size: .9rem;
+            border-spacing: 0;
+        }
+
+        .indice {
+            width: 40px;
+            text-align: center;
+            border-right: 1px solid #1b1e21;
+            padding-top: 50px;
+        }
+
     </style>
 </head>
 <body>
@@ -37,13 +69,41 @@
 <p>Cep: {{$budget->cep}}</p>
 <p>Complemento: {{$budget->complemento}}</p>
 <h3>Produtos</h3>
-<ul>
+<p>Valor total do orçamento: {{$budget->total}}</p>
+
+
     @forelse($budget->products as $product)
-        <li style="color: #0acf97;">{{ $product->mproduct->nome }}</li>
+        <div class="flex">
+            <img class="image-produto" src="{{ public_path().$product->mproduct->imagem}}">
+            <table class="tabela-produto">
+
+                <td class="indice">{{$loop->index + 1}}</td>
+
+                <td>{{$product->mproduct->nome . ' '. $product->mproduct->descricao}}
+                    <br>
+                    Linha: {{$product->mproduct->category->nome}}
+                    <br>
+                    @foreach($product->glasses as $glass)
+                        Vidro: {{$glass->nome}}
+                    @endforeach
+                    <br>
+                    @foreach($product->aluminums as $aluminum)
+                        Aluminios: {{$aluminum->perfil . ' '. $aluminum->descricao}}
+                    @endforeach
+                    <br>
+                    Largura: {{$product->largura . ' '}}
+                    Altura: {{$product->altura}}
+                    Quantidade: {{$product->qtd}}
+                    <br>
+                    Localização: {{$product->localizacao}}
+                </td>
+
+            </table>
+        </div>
     @empty
-        <li>Nenhum Produto Cadastrado.</li>
+        <p>Nenhum Produto Cadastrado.</p>
     @endforelse
-</ul>
+
 
 
 </body>
