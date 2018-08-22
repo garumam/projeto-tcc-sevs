ced2e50cf6cc62ecc31f4f29ac6d257e19aa8f19
Lucas <freirejandre@gmail.com>
2018-08-21 23:57:50 -0300

    pdf

--- vidraceiro/resources/views/dashboard/pdf/budget.blade.php
+++ vidraceiro/resources/views/dashboard/pdf/budget.blade.php
@@ -7,13 +7,18 @@
     <!--Custon CSS (está em /public/assets/site/css/certificate.css)-->
     {{--<link rel="stylesheet" href="{{ url('assets/site/css/certificate.css') }}">--}}
     <style>
-        p {
+        p,h3 {
             font-weight: 700;
             font-family: 'Raleway', sans-serif;
         }
 
         .line {
             border-bottom: 2px solid #e5e5e5;
+            height: 2px;
+        }
+        h3 {
+            background-color: #DFEBFF;
+            padding: .4rem;
         }
     </style>
 </head>
@@ -26,6 +31,12 @@
 <p>Telefone: {{$company->telefone}}</p>
 <div class="line"></div>
 
+<h3>Dados do orçamento</h3>
+<p>Nome: {{$budget->nome}}</p>
+<p>Endereço: {{$budget->endereco .' - '. $budget->bairro}}</p>
+<p>Cep: {{$budget->cep}}</p>
+<p>Complemento: {{$budget->complemento}}</p>
+<h3>Produtos</h3>
 <ul>
     @forelse($budget->products as $product)
         <li style="color: #0acf97;">{{ $product->mproduct->nome }}</li>
