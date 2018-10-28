55daaf85cf8c5ef164c8d811e9756e45cb94cbd8
Lucas Freire <freirejandre@gmail.com>
2018-10-28 17:37:07 -0300

    reajuste pdf parte 3

--- vidraceiro/resources/views/dashboard/pdf/order-comprar.blade.php
+++ vidraceiro/resources/views/dashboard/pdf/order-comprar.blade.php
@@ -7,7 +7,7 @@
     <!--Custon CSS (está em /public/assets/site/css/certificate.css)-->
     {{--<link rel="stylesheet" href="{{ url('assets/site/css/certificate.css') }}">--}}
     <style>
-        p,b, h3 {
+        p, b, h3, h4 {
             font-weight: 700;
             font-family: 'Raleway', sans-serif;
         }
@@ -17,7 +17,7 @@
             height: 2px;
         }
 
-        h3 {
+        h3, h4 {
             background-color: #DFEBFF;
             padding: .4rem;
         }
@@ -45,10 +45,12 @@
             border-spacing: 0;
             padding: 0;
         }
-        .tabela-produto tr,td{
+
+        .tabela-produto tr, td {
             border-spacing: 0;
             padding: .6rem;
         }
+
         .indice {
             width: 40px;
             padding-top: 60px;
@@ -57,23 +59,28 @@
             border-right: 1px solid #1b1e21;
 
         }
+
         .texto {
             margin: 0 auto;
             text-align: left;
             vertical-align: center;
         }
-        .total{
+
+        .total {
             width: 100%;
             height: 35px;
         }
+
         .total p {
             margin: 0;
             padding: 0;
         }
-        #texto-left{
+
+        #texto-left {
             float: left;
         }
-        #texto-right{
+
+        #texto-right {
             float: right;
         }
     </style>
@@ -108,17 +115,19 @@
         $reservasNoEstoque = $sale->storages()->get();
         //$reservasNoEstoque->shift()->pivot->qtd_reservada
     @endphp
-    <h3>Orçamento - {{$budget->nome or 'não cadastrado!'}}<hr>Materiais necessários por produto</h3>
+    <h3>Orçamento - {{$budget->nome or 'não cadastrado!'}}</h3>
+    <h3>Materiais necessários por produto</h3>
 
 
     @forelse($budget->products()->get() as $product)
-        <h4 style="background-color: #e0eafc; text-align: center;">Produto - {{$product->mproduct()->first()->nome}} | unidades: {{$product->qtd}}</h4>
+        <h4 style="text-align: center;">Produto - {{$product->mproduct()->first()->nome}} |
+            unidades: {{$product->qtd}}</h4>
         <h4>Vidros</h4>
         @forelse($product->glasses as $glass)
             <p style="margin-left: 20px;">{{'* '.$glass->nome.' '.$glass->tipo.' | m²(unidade): '.number_format(($product->largura * $product->altura), 3, '.', '')
                                                                .' | m²(total): '.number_format((($product->largura * $product->altura)*$product->qtd), 3, '.', '')}}</p>
         @empty
-            <div style="margin-left: 20px;">Nenhum vidro neste produto!</div>
+            <p style="margin-left: 20px;">Nenhum vidro neste produto!</p>
         @endforelse
         <h4>Alumínios</h4>
         @forelse($product->aluminums as $aluminum)
@@ -131,17 +140,17 @@
                     {{(ceil($aluminum->medida/6))}}
                 @endif</p>
         @empty
-            <div style="margin-left: 20px;">Nenhum alumínio neste produto!</div>
+            <p style="margin-left: 20px;">Nenhum alumínio neste produto!</p>
         @endforelse
         <h4>Componentes</h4>
         @forelse($product->components as $component)
             <p style="margin-left: 20px;">{{'* '.$component->nome.' | qtd: '.$component->qtd.' | qtd total(vezes qtd de produtos): '.$component->qtd * $product->qtd}}</p>
         @empty
-            <div style="margin-left: 20px;">Nenhum componente neste produto!</div>
+            <p style="margin-left: 20px;">Nenhum componente neste produto!</p>
         @endforelse
         <div class="line" style="margin-top: 20px;"></div>
     @empty
-        <div>Nenhum produto cadastrado neste orçamento!</div>
+        <p>Nenhum produto cadastrado neste orçamento!</p>
     @endforelse
 
     <h3>Reservado no estoque</h3>
@@ -157,7 +166,7 @@
             <p style="margin-left: 20px;">{{$component->nome.' | Quantidade reservada: '.$reserva->pivot->qtd_reservada}}</p>
         @endif
     @empty
-        <div style="margin-left: 20px;">Nada reservado no estoque para este orçamento!</div>
+        <p style="margin-left: 20px;">Nada reservado no estoque para este orçamento!</p>
     @endforelse
     <div class="line" style="margin: 20px 0 0 0;"></div>
 @endforeach
