f5b75673bbff9b2d9f5c230ea6680385a58780b9
Garumam <felipe.epr.faculdade@gmail.com>
2019-02-28 18:00:27 -0300

    Ajuste seed

--- vidraceiro/database/seeds/DatabaseSeeder.php
+++ vidraceiro/database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(ConfigurationSeeds::class);
         $this->call(PermissionSeeds::class);
         $this->call(RoleSeeds::class);
         $this->call(CarregaItens::class);
