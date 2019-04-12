ead342b85810db34ee7bd4a35ab206fbed0db77f
Garumam <felipe.epr.faculdade@gmail.com>
2019-04-12 01:02:53 -0300

    ajuste resetar senha

--- vidraceiro/app/Http/Controllers/Auth/ResetPasswordController.php
+++ vidraceiro/app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Str;
 
 class ResetPasswordController extends Controller
 {
@@ -35,5 +37,18 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    protected function resetPassword($user, $password)
+    {
+        $user->password = $password;
+
+        $user->setRememberToken(Str::random(60));
+
+        $user->save();
+
+        event(new PasswordReset($user));
+
+        $this->guard()->login($user);
     }
 }
